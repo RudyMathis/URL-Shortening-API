2d0c2bc5fe9ac5fa79bda9ed79bc05866a3356fe
RudyMathis <103608310+RudyMathis@users.noreply.github.com>
2022-08-16 12:34:11 -0400

    added url shortener api api functions properly

--- app.js
+++ app.js
@@ -8,37 +8,51 @@ menuBtn.addEventListener("click", () => {
 // api
 const shortenBtn = document.querySelector("#shorten-btn");
 const shortLinksList = document.querySelector("#short-links-list");
+const urlInput = document.querySelector("#url-input");
 
 shortenBtn.addEventListener("click", () => {
 	const fragment = document.createDocumentFragment();
 	const div = fragment.appendChild(document.createElement("div"));
+	const h2 = fragment.appendChild(document.createElement("h2"));
 	const shortLink = fragment.appendChild(document.createElement("a"));
 	const btn = fragment.appendChild(document.createElement("button"));
-	div.classList.add("link-list");
-	shortLink.classList.add("short-link");
-	div.textContent = "long link";
+
+	shortLinksList.appendChild(fragment);
+	div.appendChild(h2);
 	div.appendChild(document.createElement("hr"));
 	div.appendChild(shortLink);
-	shortLink.textContent = "a link";
-	shortLinksList.appendChild(fragment);
 	div.appendChild(btn);
-	btn.classList.add("link-btn");
-	btn.textContent = "Copy";
 
+	div.classList.add("link-list");
+	shortLink.classList.add("short-link");
+	btn.classList.add("link-btn");
+
+	h2.textContent = urlInput.value;
+
+	btn.textContent = "Copy";
 	btn.addEventListener("click", () => {
 		btn.textContent = "Copied!";
 		btn.style.backgroundColor = "var(--Purple-700)";
+
+		navigator.clipboard.writeText(shortLink.textContent);
 	});
+
+	fetch(`https://api.shrtco.de/v2/shorten?url=${urlInput.value}`)
+		.then((response) => {
+			return response.json();
+		})
+		.then((data) => {
+			const dataLink = data.result.full_short_link;
+			shortLink.textContent = `${dataLink}`;
+		})
+		.catch((err) => {
+			console.log("error retrieving data", err);
+		});
+
+	urlInput.value = "";
 });
 
-// fetch("https://api.shrtco.de/v2/shorten?url=google.com")
-// 	.then((response) => {
-// 		console.log("resolved", response);
-// 		return response.json();
-// 	})
-// 	.then((data) => {
-// 		console.log(data);
-// 	})
-// 	.catch((err) => {
-// 		console.log("error retrieving data", err);
-// 	});
+urlInput.addEventListener("input", (e) => {
+	e.preventDefault();
+	console.log(urlInput.value);
+});
