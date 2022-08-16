e1497864f726ad3f6d060e6be7c942f2e8e42274
RudyMathis <103608310+RudyMathis@users.noreply.github.com>
2022-08-16 17:41:32 -0400

    widened desktop view for larger screens made links containers responsive for desktop text wrap added propper error handleing improved short link functionality for smaller input links

--- app.js
+++ app.js
@@ -10,49 +10,77 @@ const shortenBtn = document.querySelector("#shorten-btn");
 const shortLinksList = document.querySelector("#short-links-list");
 const urlInput = document.querySelector("#url-input");
 
-shortenBtn.addEventListener("click", () => {
-	const fragment = document.createDocumentFragment();
-	const div = fragment.appendChild(document.createElement("div"));
-	const h2 = fragment.appendChild(document.createElement("h2"));
-	const shortLink = fragment.appendChild(document.createElement("a"));
-	const btn = fragment.appendChild(document.createElement("button"));
+const shortBtn = () => {
+	shortenBtn.addEventListener("click", () => {
+		if (!urlInput.value == "") {
+			const fragment = document.createDocumentFragment();
+			const div = fragment.appendChild(document.createElement("div"));
+			const p = fragment.appendChild(document.createElement("p"));
+			const shortLink = fragment.appendChild(document.createElement("a"));
+			const btn = fragment.appendChild(document.createElement("button"));
 
-	shortLinksList.appendChild(fragment);
-	div.appendChild(h2);
-	div.appendChild(document.createElement("hr"));
-	div.appendChild(shortLink);
-	div.appendChild(btn);
+			shortLinksList.appendChild(fragment);
+			div.appendChild(p);
+			div.appendChild(document.createElement("hr"));
+			div.appendChild(shortLink);
+			div.appendChild(btn);
 
-	div.classList.add("link-list");
-	shortLink.classList.add("short-link");
-	btn.classList.add("link-btn");
+			div.classList.add("link-list");
+			p.classList.add("link-text");
+			shortLink.classList.add("short-link");
+			btn.classList.add("link-btn");
 
-	h2.textContent = urlInput.value;
+			p.textContent = urlInput.value;
 
-	btn.textContent = "Copy";
-	btn.addEventListener("click", () => {
-		btn.textContent = "Copied!";
-		btn.style.backgroundColor = "var(--Purple-700)";
+			btn.textContent = "Copy";
+			btn.addEventListener("click", () => {
+				btn.textContent = "Copied!";
+				btn.style.backgroundColor = "var(--Purple-700)";
 
-		navigator.clipboard.writeText(shortLink.textContent);
+				navigator.clipboard.writeText(shortLink.textContent);
+			});
+
+			fetch(`https://api.shrtco.de/v2/shorten?url=${urlInput.value}`)
+				.then((response) => {
+					return response.json();
+				})
+				.then((data) => {
+					const dataLink = data.result.short_link;
+					const dataLink2 = data.result.short_link2;
+					if (urlInput.value.length >= 25) {
+						shortLink.textContent = `${dataLink}`;
+					} else {
+						shortLink.textContent = `${dataLink2}`;
+					}
+				})
+				.catch((err) => {
+					console.log("error retrieving data", err);
+				});
+		}
+		urlInput.value = "";
 	});
+};
 
-	fetch(`https://api.shrtco.de/v2/shorten?url=${urlInput.value}`)
-		.then((response) => {
-			return response.json();
-		})
-		.then((data) => {
-			const dataLink = data.result.full_short_link;
-			shortLink.textContent = `${dataLink}`;
-		})
-		.catch((err) => {
-			console.log("error retrieving data", err);
-		});
+urlInput.addEventListener("input", () => {
+	const regex =
+		/^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/;
 
-	urlInput.value = "";
-});
+	const invalid = document.querySelector(".invalid");
+	// if (urlInput.value == "hello") {
+	// 	shortBtn();
+	// } else {
+	// 	invalid.textContent = "Please add a link";
+	// }
+	if (urlInput.value.match(regex)) {
+		shortBtn();
+	} else {
+		invalid.textContent = "Please add a link";
+	}
 
-urlInput.addEventListener("input", (e) => {
-	e.preventDefault();
+	if (urlInput.value == "") {
+		invalid.textContent = "";
+	}
 	console.log(urlInput.value);
 });
+
+// fetch
