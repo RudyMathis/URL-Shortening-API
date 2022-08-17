c1662380e6712b4b3e773ee5b76cb044806fbe7e
RudyMathis <103608310+RudyMathis@users.noreply.github.com>
2022-08-17 14:53:26 -0400

    minor refactoring

--- app.js
+++ app.js
@@ -8,13 +8,33 @@ const menu = () => {
 };
 menu();
 
+const invalid = document.querySelector(".invalid");
+const shorten = document.querySelector(".shorten");
+
+const reset = () => {
+	if (urlInput.value == "") {
+		shorten.style.color = "var(--Purple-900";
+		shorten.style.outline = "none";
+		invalid.textContent = "";
+	}
+};
+
+const error = () => {
+	shorten.style.color = "var(--Red-400)";
+	shorten.style.outline = "2px solid var(--Red-400)";
+	invalid.textContent = "Please add a link";
+};
+
+const regex =
+	/^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/;
+
 // api
 const urlInput = document.querySelector("#url-input");
 
 const shortBtn = () => {
 	const shortenBtn = document.querySelector("#shorten-btn");
 	shortenBtn.addEventListener("click", () => {
-		if (!urlInput.value == "") {
+		if (!urlInput.value == "" && urlInput.value.match(regex)) {
 			const shortLinksList = document.querySelector("#short-links-list");
 			const fragment = document.createDocumentFragment();
 			const div = fragment.appendChild(document.createElement("div"));
@@ -61,27 +81,17 @@ const shortBtn = () => {
 				});
 		}
 		urlInput.value = "";
+		reset();
 	});
 };
 
-urlInput.addEventListener("input", () => {
-	const regex =
-		/^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/;
-
-	const invalid = document.querySelector(".invalid");
-	const shorten = document.querySelector(".shorten");
-
+const userInput = () => {
 	if (urlInput.value.match(regex)) {
 		shortBtn();
 	} else {
-		shorten.style.color = "var(--Red-400)";
-		shorten.style.outline = "2px solid var(--Red-400)";
-		invalid.textContent = "Please add a link";
+		error();
 	}
+	reset();
+};
 
-	if (urlInput.value == "") {
-		shorten.style.color = "var(--Purple-900";
-		shorten.style.outline = "none";
-		invalid.textContent = "";
-	}
-});
+urlInput.addEventListener("input", userInput);
